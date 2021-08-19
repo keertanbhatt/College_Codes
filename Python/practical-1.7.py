f2175d88af0d2450bb085b668fc35d053dcac745
Keertan Bhatt <71007100+keertanbhatt@users.noreply.github.com>
2021-08-19 16:31:48 +0530

    practical-1.7.py
    write a Python program to find first n prime numbers.

---
+++ Python/practical-1.7.py
@@ -0,0 +1,16 @@
+Number = int(input(" Enter a Number: "))
+
+
+
+print("Prime numbers between", 1, "and", Number, "are:")
+
+
+
+for num in range(1, Number + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+
